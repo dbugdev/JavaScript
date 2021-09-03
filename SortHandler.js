382b6427ad26b7eac64412f0a40a20ddd957e7a9
Alok <dbugdev@gmail.com>
2021-09-03 12:47:55 +0530

    code format

--- SortHandler.js
+++ SortHandler.js
@@ -1,46 +1,37 @@
-class SortHandler{
-    static selectionSort(arr){
-        for(let i=0;i<arr.length-1;i++)
-        {
-            for(let j=i+1;j<arr.length;j++)
-            {
-                if(arr[i]>arr[j])
-                {
-                    [arr[i],arr[j]]=[arr[j],arr[i]];
-                }
-            }
+class SortHandler {
+  static selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+          [arr[i], arr[j]] = [arr[j], arr[i]];
         }
-        return arr
+      }
     }
-    static bubbleSort(arr){
-        for(let i=0;i<arr.length;i++)
-        {
-            for(let j=0;j<arr.length-1-i;j++)
-            {
-                if(arr[j]>arr[j+1])
-                {
-                    [arr[j],arr[j+1]]=[arr[j+1],arr[j]];
-                }
-            }
+    return arr;
+  }
+  static bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - 1 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         }
-        return arr
+      }
     }
-    static insersionSort(arr){
-        for(let i =0;i<arr.length-1;i++){
-            sort(i+1)
-         }
-         function sort(last){
-            if(last===0||arr[last]>arr[last-1]) {
-                return;
-            }
-            
-            [arr[last],arr[last-1]]= [arr[last-1],arr[last]];
-         
-            sort(--last);
-        }
-        return arr
+    return arr;
+  }
+  static insersionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+      sort(i + 1);
     }
-    
+    function sort(last) {
+      if (last === 0 || arr[last] > arr[last - 1]) {
+        return;
+      }
+      [arr[last], arr[last - 1]] = [arr[last - 1], arr[last]];
+      sort(--last);
+    }
+    return arr;
+  }
 }
 
-module.exports = SortHandler
+module.exports = SortHandler;
